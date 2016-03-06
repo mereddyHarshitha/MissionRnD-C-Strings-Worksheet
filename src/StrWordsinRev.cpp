a92b1b5afd9f43080dad97e76a3ef857d7753cf6
mereddyHarshitha <mereddyHarshitha@missionrnd.com>
2016-03-06 16:13:49 +0530

    Submit Commit

--- src/StrWordsinRev.cpp
+++ src/StrWordsinRev.cpp
@@ -14,4 +14,39 @@ NOTES: Don't create new string.
 
 void str_words_in_rev(char *input, int len){
 	
+	int i, k = 0, j, temp;
+	for (i = 0, j = 0; j<len; j++){
+
+		if ((input[j] == ' ') || (j == len - 1)){
+			if (j<len - 1)
+				k = j - 1;
+			else
+				k = j;
+
+			while (i<k){
+				temp = input[i];
+				input[i] = input[k];
+				input[k] = temp;
+				i++;
+				k--;
+			}
+			i = j + 1;
+		}
+	}
+
+	for (i = 0, j = 0; j<len; j++){
+
+		if (j == len - 1){
+			k = j;
+
+			while (i<k){
+				temp = input[i];
+				input[i] = input[k];
+				input[k] = temp;
+				i++;
+				k--;
+			}
+			i = j + 1;
+		}
+	}
 }
