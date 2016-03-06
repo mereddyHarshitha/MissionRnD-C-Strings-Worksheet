b068fcb81820fc1b3c36ed632a8c2b2e91ec5417
mereddyHarshitha <mereddyHarshitha@missionrnd.com>
2016-03-06 11:53:03 +0530

    Submit Commit

--- src/NumbertoStr.cpp
+++ src/NumbertoStr.cpp
@@ -18,8 +18,68 @@ NOTES: Don't create new string.
 */
 
 #include <stdio.h>
+#include <math.h>
 
+void reverse(char *str, int len){
+
+	int i, j, temp;
+	for (i = 0, j = len - 1; i < j; i++, j--){
+
+		temp = str[i];
+		str[i] = str[j];
+		str[j] = temp;
+	}
+}
+
+
+int toString(int number, char str[]){
+
+	int i = 0;
+	while (number){
+
+		str[i++] = (number % 10) + '0';
+		number = number / 10;
+	}
+	reverse(str, i);
+	str[i] = '\0';
+	return i;
+}
 
 void number_to_str(float number, char *str,int afterdecimal){
+
+	int integer, i;
+	float fraction;
+
+	if (number >= 0){
+
+		integer = (int)number;
+		i = toString(integer, str);
+
+		fraction = number - (float)integer;
+
+		if (afterdecimal != 0){
+
+			str[i] = '.';
+			fraction = fraction * pow((double)10, afterdecimal);
+			toString(fraction, str + i + 1);
+		}
+
+	}
+	else{
+
+		str[0] = '-';
+
+		integer = -((int)number);
+		i = toString(integer, str + 1);
+
+		fraction = -(number + integer);
+
+		if (afterdecimal != 0)
+		{
+			str[i + 1] = '.';
+			fraction = fraction * pow((double)10, afterdecimal);
+			toString(fraction, str + i + 2);
+		}
+	}
 	
 }
